5d21e5d28fc0c7e3ad9e813dbc46d54a149bf98a
dioh0922 <diohound@gmail.com>
2025-08-17 09:31:47 +0900

    ヘッダ調整

--- extApi/delete.php
+++ extApi/delete.php
@@ -1,7 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+//header("Access-Control-Allow-Methods: DELETE, OPTIONS");
+//header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204); // No Content
     exit();
