299098b1f33d8ae385c63de3230330891eada1a5
dioh0922 <diohound@gmail.com>
2025-11-15 09:57:01 +0900

    googleログインまで追加

---
+++ src/components/authButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="loginWithGoogle">
+    Google ログイン
+  </button>
+</template>
+
+<script setup>
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "@/firebase";
+
+const provider = new GoogleAuthProvider();
+
+const loginWithGoogle = async () => {
+  try {
+    await signInWithPopup(auth, provider);
+    console.log("Google ログイン成功");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+</script>
+
+<style>
+
+</style>
